4fd952c802e1c64f77d454375a3acddccd18b24d
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-05-13 15:45:54 +0700

    mendeklarasikan parameter dan obyek dalam kelas Mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -4,5 +4,37 @@ using namespace std;
 
 class Mahasiswa
 {
+public:
+    int nim;
+    string nama;
+    
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
 
+    };
+
+    Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    }
+
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    }
+
+    void cetak()
+    {
+        cout << "NIM    = " << nim << endl;
+        cout << "nama   = " << nama << endl;
+        cout << endl;
+    }
 };
+
